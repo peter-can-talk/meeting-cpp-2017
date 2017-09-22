f767b37bdcbf14dc10bf80884ba4e0ca8051a6d8
goldsborough <peter@goldsborough.me>
2017-09-22 19:56:23 -0400

    Fix CUDNN convolution

--- code/cudnn/conv.cu
+++ code/cudnn/conv.cu
@@ -4,28 +4,53 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+#define checkCUDNN(expression)                               \
+  {                                                          \
+    cudnnStatus_t status = (expression);                     \
+    if (status != CUDNN_STATUS_SUCCESS) {                    \
+      std::cerr << "Error on line " << __LINE__ << ": "      \
+                << cudnnGetErrorString(status) << std::endl; \
+      std::exit(EXIT_FAILURE);                               \
+    }                                                        \
+  }
+
 cv::Mat load_image(const char* image_path) {
   cv::Mat image = cv::imread(image_path, CV_LOAD_IMAGE_COLOR);
   image.convertTo(image, CV_32FC3);
   cv::normalize(image, image, 0, 1, cv::NORM_MINMAX);
+  std::cerr << "Input Image: " << image.rows << " x " << image.cols << " x 3"
+            << std::endl;
   return image;
 }
 
-#define checkCUDNN(status)                                 \
-  if (status != CUDNN_STATUS_SUCCESS) {                    \
-    std::cerr << "Error on line " << __LINE__ << ": "      \
-              << cudnnGetErrorString(status) << std::endl; \
-    std::exit(EXIT_FAILURE);                               \
-  }
+void save_image(const char* output_filename,
+                float* buffer,
+                int height,
+                int width) {
+  cv::Mat output_image(height, width, CV_32FC3, buffer);
+  // Make negative values zero.
+  cv::threshold(output_image,
+                output_image,
+                /*threshold=*/0,
+                /*maxval=*/0,
+                cv::THRESH_TOZERO);
+  cv::normalize(output_image, output_image, 0.0, 255.0, cv::NORM_MINMAX);
+  output_image.convertTo(output_image, CV_8UC3);
+  cv::imwrite(output_filename, output_image);
+  std::cerr << "Wrote output to " << output_filename << std::endl;
+}
 
 int main(int argc, const char* argv[]) {
   if (argc < 2) {
-    std::cerr << "usage: conv <image> [gpu=0]" << std::endl;
+    std::cerr << "usage: conv <image> [gpu=0] [sigmoid=0]" << std::endl;
     std::exit(EXIT_FAILURE);
   }
 
   int gpu_id = (argc > 2) ? std::atoi(argv[2]) : 0;
-  std::cout << "GPU: " << gpu_id << std::endl;
+  std::cerr << "GPU: " << gpu_id << std::endl;
+
+  bool with_sigmoid = (argc > 3) ? std::atoi(argv[3]) : 0;
+  std::cerr << "With sigmoid: " << std::boolalpha << with_sigmoid << std::endl;
 
   cv::Mat image = load_image(argv[1]);
 
@@ -75,7 +100,7 @@ int main(int argc, const char* argv[]) {
                                                    &height,
                                                    &width));
 
-  std::cout << "Output Image: " << height << " x " << width << " x " << channels
+  std::cerr << "Output Image: " << height << " x " << width << " x " << channels
             << std::endl;
 
   cudnnTensorDescriptor_t output_descriptor;
@@ -108,7 +133,7 @@ int main(int argc, const char* argv[]) {
                                                      output_descriptor,
                                                      convolution_algorithm,
                                                      &workspace_bytes));
-  std::cout << "Workspace size: " << (workspace_bytes / 1048576) << "MB"
+  std::cerr << "Workspace size: " << (workspace_bytes / 1048576.0) << "MB"
             << std::endl;
   assert(workspace_bytes > 0);
 
@@ -164,33 +189,28 @@ int main(int argc, const char* argv[]) {
                                      output_descriptor,
                                      d_output));
 
-  cudnnActivationDescriptor_t activation_descriptor;
-  checkCUDNN(cudnnCreateActivationDescriptor(&activation_descriptor));
-  checkCUDNN(cudnnSetActivationDescriptor(activation_descriptor,
-                                          CUDNN_ACTIVATION_SIGMOID,
-                                          CUDNN_PROPAGATE_NAN,
-                                          /*relu_coef=*/0));
-  checkCUDNN(cudnnActivationForward(cudnn,
-                                    activation_descriptor,
-                                    &alpha,
-                                    output_descriptor,
-                                    d_output,
-                                    &beta,
-                                    output_descriptor,
-                                    d_output));
+  if (with_sigmoid) {
+    cudnnActivationDescriptor_t activation_descriptor;
+    checkCUDNN(cudnnCreateActivationDescriptor(&activation_descriptor));
+    checkCUDNN(cudnnSetActivationDescriptor(activation_descriptor,
+                                            CUDNN_ACTIVATION_SIGMOID,
+                                            CUDNN_PROPAGATE_NAN,
+                                            /*relu_coef=*/0));
+    checkCUDNN(cudnnActivationForward(cudnn,
+                                      activation_descriptor,
+                                      &alpha,
+                                      output_descriptor,
+                                      d_output,
+                                      &beta,
+                                      output_descriptor,
+                                      d_output));
+    cudnnDestroyActivationDescriptor(activation_descriptor);
+  }
 
   float* h_output = new float[image_bytes];
   cudaMemcpy(h_output, d_output, image_bytes, cudaMemcpyDeviceToHost);
 
-  cv::Mat output_image(height, width, CV_32FC3, h_output);
-  cv::normalize(output_image, output_image, 0.0, 255.0, cv::NORM_MINMAX);
-
-  output_image.convertTo(output_image, CV_8UC3);
-
-  const char* output_filename = "out.png";
-  cv::imwrite(output_filename, output_image);
-  std::cout << "Wrote output to " << output_filename << std::endl;
-
+  save_image("out.png", h_output, height, width);
 
   delete[] h_output;
   cudaFree(d_kernel);
@@ -202,7 +222,6 @@ int main(int argc, const char* argv[]) {
   cudnnDestroyTensorDescriptor(output_descriptor);
   cudnnDestroyFilterDescriptor(kernel_descriptor);
   cudnnDestroyConvolutionDescriptor(convolution_descriptor);
-  checkCUDNN(cudnnDestroyActivationDescriptor(activation_descriptor));
 
   cudnnDestroy(cudnn);
 }
